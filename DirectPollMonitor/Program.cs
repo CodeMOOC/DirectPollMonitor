11045928312732952b4a0448294ebc81813a7b68
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-12 16:58:15 +0200

    Fix code length

--- DirectPollMonitor/Program.cs
+++ DirectPollMonitor/Program.cs
@@ -22,7 +22,8 @@ namespace DirectPollMonitor {
                 Console.ForegroundColor = prevColor;
             }
 
-            Console.Error.WriteLine("Usage: {0}.exe <DirectPoll URL>", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+            Console.Error.WriteLine("Usage: {0} <DirectPoll URL>",
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
 
             Environment.Exit(1);
         }
